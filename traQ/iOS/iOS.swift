5413486f761c7b11d9501a4637bf8caa6cec03df
ras0q <ras0q@users.noreply.github.com>
2024-11-08 21:26:47 +0900

    :truck: Move View to Package

--- traQ/iOS/iOS.swift
+++ traQ/iOS/iOS.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import SwiftData
 
 @main
-struct traQApp: App {
+struct IOSApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Item.self,
