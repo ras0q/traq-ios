cf96589af5b583a4e771758add26806db9dc7ee1
ras0q <ras0q@users.noreply.github.com>
2024-11-09 19:03:21 +0900

    :bug: Fix date formatter

--- Sources/TraqAPI/TraqAPIClient.swift
+++ Sources/TraqAPI/TraqAPIClient.swift
@@ -14,7 +14,46 @@ package let traqServerURL = {
 package let traqAPIClient = Client(
     serverURL: traqServerURL,
     configuration: Configuration(
-        dateTranscoder: ISO8601DateTranscoder(options: .withFractionalSeconds)
+        dateTranscoder: CustomDateTranscoder()
     ),
     transport: URLSessionTransport()
 )
+
+
+struct CustomDateTranscoder: DateTranscoder {
+    let dateFormatter: DateFormatter
+    let dateFormatterWithFractional: DateFormatter
+
+    init() {
+        dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY-MM-dd'T'HH:mm:ssZ"
+        dateFormatter.locale = .current
+        dateFormatter.timeZone = .current
+
+        dateFormatterWithFractional = DateFormatter()
+        dateFormatterWithFractional.dateFormat = "YYYY-MM-dd'T'HH:mm:ss.SSSSSSZ"
+        dateFormatterWithFractional.locale = .current
+        dateFormatterWithFractional.timeZone = .current
+    }
+
+    func encode(_ date: Date) throws -> String {
+        dateFormatterWithFractional.string(from: date)
+    }
+
+    func decode(_ dateString: String) throws -> Date {
+        if let date = dateFormatterWithFractional.date(from: dateString) {
+            return date
+        }
+
+        if let date = dateFormatter.date(from: dateString) {
+            return date
+        }
+
+        throw DecodingError.dataCorrupted(
+            .init(
+                codingPath: [],
+                debugDescription: "Expected date string to be ISO8601-formatted."
+            )
+        )
+    }
+}
